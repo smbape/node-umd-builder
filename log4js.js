798d1a732dbf9ff2c0c3f60fa520409bf9397bfc
Stéphane MBAPE <rose.martin-8og3raxw@yopmail.com>
2015-10-02 21:13:26 +0000

    fix log folder location
    git-svn-id: http://127.0.0.1:20080/svn/buma/branches/tutorial/library/node-umd-builder@736 b2333513-9f29-43eb-bce0-aa7d4792c706

--- log4js.js
+++ log4js.js
@@ -8,7 +8,12 @@ var configFile, log4js, logFolder, reloadSecs;
 if (!global.log4js) {
     log4js = global.log4js || (global.log4js = require('log4js'));
     configFile = sysPath.join(__dirname, 'log4js.json');
-    logFolder = sysPath.join(__dirname, '..', 'logs');
+    if (/[\\\/]node_modules[\\\/]/.test(__dirname)) {
+        logFolder = sysPath.resolve(__dirname, '..', '..');
+    } else {
+        logFolder = __dirname;   
+    }
+
     mkdirp.sync(logFolder);
 
     if (process.argv[2] === 'watch') {
